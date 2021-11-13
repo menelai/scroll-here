e2f3598029ef275c57950bd726c361842a40396f
kovalenko <kovalenko@gmail.com>
2021-11-13 22:30:42 +0300

    fix(drag-scroll): style
    fix(datetime-picker): css

--- projects/drag-scroll/src/lib/drag-scroll.directive.ts
+++ projects/drag-scroll/src/lib/drag-scroll.directive.ts
@@ -1,22 +1,26 @@
-import {Directive, ElementRef, HostBinding, HostListener, Input, OnDestroy} from '@angular/core';
+import {Directive, ElementRef, HostBinding, HostListener, Input, OnDestroy, OnInit} from '@angular/core';
 import {DomSanitizer, SafeStyle} from '@angular/platform-browser';
 
 @Directive({
   selector: '[dragScroll]'
 })
-export class DragScrollDirective implements OnDestroy {
-  @HostBinding('[attr.style]')
-  get myStyle(): SafeStyle {
-    return this.sanitizer.bypassSecurityTrustStyle('cursor: grab');
-  }
-  @Input('dragScroll') private _enabled!: boolean;
+export class DragScrollDirective implements OnInit, OnDestroy {
+  @HostBinding('[attr.style]') myStyle!: SafeStyle;
+  @Input('dragScroll') _enabled!: boolean;
   private _pushed = false;
   private lastClientX!: number;
   private startClientX!: number;
   private lastClientY!: number;
   private startClientY!: number;
 
-  constructor(private elRef: ElementRef, private sanitizer: DomSanitizer) {}
+  constructor(
+    private elRef: ElementRef,
+    private sanitizer: DomSanitizer
+  ) {}
+
+  ngOnInit(): void {
+    this.myStyle = this.sanitizer.bypassSecurityTrustStyle('cursor: grab');
+  }
 
   ngOnDestroy(): void {
     window.removeEventListener('mousemove', this._mouseMove);
