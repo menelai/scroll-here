0e65680076fc1a94f817a1fd5c2a9729f8bea678
kovalenko <kovalenko@gmail.com>
2025-05-03 13:53:41 +0300

    datepicker

--- projects/material-datetime-picker/src/public-api.ts
+++ projects/material-datetime-picker/src/public-api.ts
@@ -1,2 +1,1 @@
-export * from './lib/datetime-picker/datetime-picker.component';
-export * from './lib/moment-datetime-picker/moment-datetime-picker.component';
+export * from './lib/luxon-datetime-picker/datetime-picker.component';
